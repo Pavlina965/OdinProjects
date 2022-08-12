9e1a2063b06900869b0b94fcbdb366408df46beb
Pavlina <pavlina965@gmail.com>
2022-08-12 13:44:58 +0200

    calculator- begining part two

--- OdinCalculator/script.js
+++ OdinCalculator/script.js
@@ -1,7 +1,3 @@
-const btnAdd = document.getElementById('plus')
-const btnMinus = document.getElementById('minus')
-const btnMultiply = document.getElementById('star')
-const btnDivide = document.getElementById('divide')
 const btnDelete = document.getElementById('delete')
 const btnClear = document.getElementById('clear')
 const btnEqual = document.getElementById('equals')
@@ -19,141 +15,96 @@ let isResult = false;
 let displayNum = ""
 let displayOper = ""
 
-function assignNumber(button) {
+function assemble(button) {
     btnNum = parseInt(button.getAttribute('data-num'));
     btnOper = button.getAttribute('data-oper')
     const btnClass = button.className;
+    //console.log('chosed numbers '+numChosed,'chosed operator '+operatorChosed,'clicked = '+isResult)
     if (numChosed === false && operatorChosed === false && btnClass === 'Number') {
-        if (!isResult) {
             displayNum += btnNum
             num1 = parseInt(displayNum)
-            //console.log(num1)
             display.textContent = num1
-            console.log(isResult + "1")
         }
 
-        //
-    }
-    if (/*operatorChosed === false &&*/ btnClass === 'Operator') {
-        //console.log(btnOper)
+    if (btnClass === 'Operator') {
         operatorChosed = true;
         operator = btnOper;
-        //console.log(btnOper)
+        console.log(operator)
         if (num1 != null) {
             numChosed = true;
             displayNum = ''
         }
     }
-    if (numChosed && btnClass === 'Number' && !isResult) {
+    if (numChosed && operatorChosed && btnClass === 'Number') {
         displayNum += btnNum
         num2 = parseInt(displayNum)
-        //console.log(displayNum)
         display.textContent = num2
 
     }
 }
 
 function equals() {
-    if (numChosed && operatorChosed && !isResult) {
+    console.log(operator)
+    if (operatorChosed) {
         switch (operator) {
             case '+':
                 result = num1 + num2
-                displayNum = result
-                console.log(displayNum)
-                isResult = true;
-                num2='';
-                num1=result;
-                operatorChosed = false
-                display.textContent=displayNum
+                console.log(result)
+                display.textContent =result
+                num1 = result
+                console.log(num2 + 'num2')
+                console.log(displayNum+'displaynum')
+                num2=''
+                displayNum = ''
+                operatorChosed=false
                 break
             case '-':
                 result = num1 - num2
-                displayNum = result
                 console.log(displayNum)
                 isResult = true;
-                num2='';
-                num1=result;
+                num2 = '';
+                num1 = result;
                 operatorChosed = false
-                display.textContent=displayNum
+                display.textContent = displayNum
                 break
             case '*':
                 result = num1 * num2
                 displayNum = result
                 console.log(displayNum)
                 isResult = true;
-                num2='';
-                num1=result;
+                num2 = '';
+                num1 = result;
                 operatorChosed = false
-                display.textContent=displayNum
+                display.textContent = displayNum
                 break
             case '/':
                 result = num1 / num2
                 displayNum = result
                 console.log(displayNum)
                 isResult = true;
-                num2='';
-                num1=result;
+                num2 = '';
+                num1 = result;
                 operatorChosed = false
-                display.textContent=displayNum
+                display.textContent = displayNum
         }
     } else {
         console.log('NO!')
     }
-//if(numChosed && operatorChosed)
-//    switch (btnOper) {
-//        case '+':
-//            displayNum = num1 + num2
-//            display.textContent = displayNum
-//            break
-//        case '-':
-//            displayNum = num1 - num2
-//            display.textContent = displayNum
-//            break
-//        case '*':
-//            displayNum = num1 * num2
-//            display.textContent = displayNum
-//            break
-//        case '/':
-//            displayNum = num1 / num2
-//            display.textContent = displayNum
-//    }
 }
-
+function clear(){
+    num1=''
+    num2=''
+    operator=''
+    displayNum=''
+    display.textContent='0'
+    numChosed=false
+    isResult=false
+    operatorChosed=false
+}
 btnEqual.addEventListener('click', () => equals())
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-        assignNumber(button)
+        assemble(button)
     })
 })
-/*numbers.forEach(button => {
-    button.addEventListener('click', () => {
-        const btnClass = button.className;
-
-
-        if (btnClass === 'Number') {
-
-            btnNum = parseInt(button.getAttribute('data-num'));
-            if (operatorChosed === false) {
-                numChosed = true;
-                num1 += btnNum;
-                return display.textContent = num1
-            }
-            if (operatorChosed === true) {
-                num2 += btnNum
-            }
-        }
-
-        if (btnClass === 'Operator') {
-            operatorChosed = true;
-            btnOper = button.getAttribute('data-num')
-        }
-        console.log(numChosed)
-        console.log(displayNum)
-        display.textContent = displayNum
-
-
-        //console.log(typeof btnNum)
-
-
-    })
-})*/
+btnClear.addEventListener('click', ()=>clear())
